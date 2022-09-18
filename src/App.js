e4a75863748e273af04a428a077c88590bc0bf81
veeramani1202 <veeramanikutty1@gmail.com>
2022-09-18 12:47:56 +0530

    change

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './component/First'
+import Message from './component/Message'
+
+function App() {
+  return (
+    <div className="App">
+<Greet />
+      <Message />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/First.js
@@ -0,0 +1,7 @@
+import React from 'react'
+//function Greet() {
+  //  return <h1>Veeramani</h1>
+//}
+const  Greet = ( ) => <h1>mani</h1>
+export default Greet
+ 
